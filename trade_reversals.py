b30de8266abc64f5e6af30c67af0d54366137689
Michael Mott <michael@mott.digital>
2024-01-05 18:33:03 -0500

    added moving average crossover strategy

--- trade_reversals.py
+++ trade_reversals.py
@@ -5,6 +5,7 @@ import numpy as np
 import talib as ta
 import time
 import win32com.client
+import argparse
 
 
 from trade_utils import get_symbol_data, create_opening_trade
@@ -15,51 +16,76 @@ def open_position(symbol, data):
     # look for a trade
     current_cdl = data.iloc[0]
 
-    print(data.head(5)[["ptn_bullish_engulfing", "ptn_bearish_engulfing", "ind_rsi"]])
+    print(data.head(5)[["close", "ind_ma_cross", "ind_sma9", "ind_sma21"]])
 
     number_of_lots = .01
     
-    # convert to numpy array so it easier to access previous rsi values
-    ind_rsi = data.ind_rsi.to_numpy()
-
-    rsi_overbought_threshold = 65
-    rsi_oversold_threshold = 35
-
-    is_down_trend = (
-        True if ind_rsi[1] < rsi_oversold_threshold or ind_rsi[2] < rsi_oversold_threshold or ind_rsi[3] < rsi_oversold_threshold
-        else False
-    )
-
-    is_up_trend = (
-        True if ind_rsi[1] > rsi_overbought_threshold or ind_rsi[2] > rsi_overbought_threshold or ind_rsi[3] > rsi_overbought_threshold
-        else False
-    )
-
-    if ( 
-            (current_cdl.ptn_bullish_engulfing or current_cdl.ptn_morningstar) and
-            current_cdl.ind_rsi > rsi_oversold_threshold and
-            is_down_trend
-        ):
+    is_test_buy = False
+    if is_test_buy or current_cdl.ind_ma_cross == 1:
+        #buy
         if IS_SPEECH_ENABLED:
-            speaker.Speak('alert BUY')
+                speaker.Speak('alert BUY')
+
         print("Buy")
         current_price = mt5.symbol_info_tick(symbol).ask
-        ts_amount = current_cdl.ind_atr * 2
-        position = create_opening_trade(symbol, "buy", number_of_lots, ts_amount)
+        sl_amount = current_cdl.ind_atr * 2
+        position = create_opening_trade(symbol, "buy", number_of_lots, sl_amount)
         return position
 
-    if (
-        (current_cdl.ptn_bearish_engulfing or current_cdl.ptn_eveningstar) and 
-        current_cdl.ind_rsi < rsi_oversold_threshold and
-        is_up_trend
-        ):
+    elif current_cdl.ind_ma_cross == -1:
+        #sell
         if IS_SPEECH_ENABLED:
-            speaker.Speak('alert SELL')
+                speaker.Speak('alert SELL')
         print("Sell")
         current_price = mt5.symbol_info_tick(symbol).bid
-        ts_amount = current_cdl.ind_atr * 2
-        position = create_opening_trade(symbol, "sell", number_of_lots, ts_amount)
+        sl_amount = current_cdl.ind_atr * 2
+        position = create_opening_trade(symbol, "sell", number_of_lots, sl_amount)
         return position
+
+
+    strategy_engulfing = False 
+    if strategy_engulfing: 
+        # convert to numpy array so it easier to access previous rsi values
+        ind_rsi = data.ind_rsi.to_numpy()
+
+        rsi_overbought_threshold = 60
+        rsi_oversold_threshold = 40
+
+        is_down_trend = (
+            True if ind_rsi[1] < rsi_oversold_threshold or ind_rsi[2] < rsi_oversold_threshold or ind_rsi[3] < rsi_oversold_threshold
+            else False
+        )
+
+        is_up_trend = (
+            True if ind_rsi[1] > rsi_overbought_threshold or ind_rsi[2] > rsi_overbought_threshold or ind_rsi[3] > rsi_overbought_threshold
+            else False
+        )
+
+        if ( 
+                (current_cdl.ptn_bullish_engulfing_1 or current_cdl.ptn_morningstar) #and
+                #current_cdl.ind_rsi > rsi_oversold_threshold and
+                #is_down_trend
+            ):
+            if IS_SPEECH_ENABLED:
+                speaker.Speak('alert BUY')
+            print("Buy")
+            current_price = mt5.symbol_info_tick(symbol).ask
+            sl_amount = current_cdl.ind_atr * 1.5
+            position = create_opening_trade(symbol, "buy", number_of_lots, sl_amount)
+            return position
+
+        if (
+            (current_cdl.ptn_bearish_engulfing_1 or current_cdl.ptn_eveningstar) #and 
+            #current_cdl.ind_rsi < rsi_oversold_threshold and
+            #is_up_trend
+            ):
+            if IS_SPEECH_ENABLED:
+                speaker.Speak('alert SELL')
+            print("Sell")
+            current_price = mt5.symbol_info_tick(symbol).bid
+            sl_amount = current_cdl.ind_atr * 1.5
+            position = create_opening_trade(symbol, "sell", number_of_lots, sl_amount)
+            return position
 
     return None
 
@@ -77,26 +103,46 @@ def manage_position(position, data):
         print( "no open positions")
         return None
     
-    ts_amount = position["ts_amount"]
-    #print(position["symbol"], ts_amount)
+    
+    print(position)
+    sl_amount = position["sl_amount"]
+    #print(position["symbol"], sl_amount)
 
     old_sl = position["sl"]
     move_sl = False
 
+    current_cdl = data.iloc[0]
+
     # get current price
     if position["long_or_short"] == "long":
         current_price = mt5.symbol_info_tick(symbol).ask
+        
+        # standard trailing stop
+        # new_sl = current_price - sl_amount
+        
+        if (
+            current_cdl.low > position["price"] and
+            current_cdl.low > current_cdl.ind_sma15
+            ):
+            new_sl = current_cdl.ind_sma15 
 
-        new_sl = current_price - ts_amount
-        if new_sl > old_sl:
-            move_sl = True
+            if new_sl > old_sl:
+                move_sl = True
             
     else: 
         current_price = mt5.symbol_info_tick(symbol).bid
 
-        new_sl = current_price + ts_amount
-        if new_sl < old_sl:
-            move_sl = True
+        # standard trailing stop
+        # new_sl = current_price + sl_amount
+        
+        if (
+            current_cdl.high < position["price"] and 
+            current_cdl.high < current_cdl.ind_sma15
+            ):
+            new_sl = current_cdl.ind_sma15
+
+            if new_sl < old_sl:
+                move_sl = True
 
     if move_sl:
         if IS_SPEECH_ENABLED:
@@ -118,6 +164,20 @@ def manage_position(position, data):
     return position
     
 
+
+# ---------------------------------------------------------
+#  Main 
+# ---------------------------------------------------------
+
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-s", "--symbol", help="symbol")
+
+args = argParser.parse_args()
+print(args.symbol)
+
+symbol = args.symbol
+
 IS_SPEECH_ENABLED = True
 
 # connect to MetaTrader 5
@@ -130,8 +190,6 @@ print('Connected Version: ' + str(mt5.version()))
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 if IS_SPEECH_ENABLED:
     speaker.Speak('connected to Meta trader')
-
-symbol = "ETHUSD"
 
 current_time = 0
 previous_time = 0
--- trade_utils.py
+++ trade_utils.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 import csv
 
-def get_symbol_data(symbol, closed_candles_only=True, periods=300, timeframe=mt5.TIMEFRAME_M5, with_indicators=True):
+def get_symbol_data(symbol, closed_candles_only=True, periods=300, timeframe=mt5.TIMEFRAME_M5):
     #print("get symbol history")
 
     data = pd.DataFrame(mt5.copy_rates_from_pos(
@@ -27,15 +27,31 @@ def get_symbol_data(symbol, closed_candles_only=True, periods=300, timeframe=mt5
 
        )
     
+    # basic candle statistics
+    data = (data
+        .assign(cdl_size = abs(data.high - data.low))
+        .assign(cdl_body_size = abs(data.close - data.open))
+        .assign(cdl_up = np.where(data.open < data.close, True, False))
+        .assign(cdl_down = np.where(data.open > data.close, True, False))
+       )
+
+    data = (data 
+        .assign(cdl_body_perc = data.cdl_body_size / data.cdl_size )
+       )
+
     #data.rename(columns={"open": "Open", "close": "Close", "low": "Low", "high": "High", "volume": "Volume"}, inplace=True)
 
     # Add technical indicators
     data  = (data
              .assign(ind_rsi = ta.RSI(data.close, 14),
                     ind_adx = ta.ADX(data.high, data.low, data.close, timeperiod=14),
-                    ind_atr = ta.ATR(data.high, data.low, data.close, timeperiod=14)
-                    
+                    ind_atr = ta.ATR(data.high, data.low, data.close, timeperiod=14),
+                    ind_sma9 = data.close.rolling(9).mean(),
+                    ind_sma15 = data.close.rolling(15).mean(),
+                    ind_sma21 = data.close.rolling(21).mean(),
+                   
                 )
+           
             .assign(
                 ptn_bullish_engulfing = np.where(ta.CDLENGULFING(data.open, data.high, data.low, data.close) == 100, True, False),
                 ptn_bearish_engulfing = np.where(ta.CDLENGULFING(data.open, data.high, data.low, data.close) == -100, True, False),
@@ -44,9 +60,11 @@ def get_symbol_data(symbol, closed_candles_only=True, periods=300, timeframe=mt5
 
                 )
             )
-
-    data["ind_rsi_overbought"] = ""
     
+    data = data.assign( ind_ma_bullish = np.where(data.ind_sma9 > data.ind_sma21, 1.0, 0.0))
+
+    data = data.assign( ind_ma_cross = data.ind_ma_bullish.diff())
+            
     # most recent bar is incomplete, we need to remove it
     # we only want to use completed candles
     if closed_candles_only:
@@ -58,19 +76,37 @@ def get_symbol_data(symbol, closed_candles_only=True, periods=300, timeframe=mt5
     
     #print(data.loc[:, data.columns.str.startswith( ('ind_', 'ptn_') )].query("ptn_bullish_engulfing or ptn_bearish_engulfing"))
    
+
+    previous_cdl = data.shift(-1)
+    data["ptn_bullish_engulfing_1"] = np.where(
+            (data.open <= previous_cdl.close) &
+            (data.close >= previous_cdl.open) &
+            (data.cdl_up & previous_cdl.cdl_down),
+            True, False
+        )
+
+    data["ptn_bearish_engulfing_1"] = np.where(
+            (data.open >= previous_cdl.close) &
+            (data.close <= previous_cdl.open) &
+            (data.cdl_down & previous_cdl.cdl_up), 
+            True, False
+        )
+    
+
+
     return data
 
 
-def create_opening_trade(symbol, order_type, number_of_lots,ts_amount):
+def create_opening_trade(symbol, order_type, number_of_lots,sl_amount):
     if order_type == "buy":
         type = mt5.ORDER_TYPE_BUY
         price = mt5.symbol_info_tick(symbol).ask
-        sl = price - ts_amount 
+        sl = price - sl_amount 
         long_or_short = "long"
     else:
         type = mt5.ORDER_TYPE_SELL
         price = mt5.symbol_info_tick(symbol).bid
-        sl = price + ts_amount
+        sl = price + sl_amount
         long_or_short = "short"
 
     request = {
@@ -100,7 +136,7 @@ def create_opening_trade(symbol, order_type, number_of_lots,ts_amount):
         "long_or_short": long_or_short,
         "price": new_ticket.price,
         "lots": new_ticket.volume,
-        "ts_amount": ts_amount,
+        "sl_amount": sl_amount,
         "sl": sl
     }
 
@@ -122,17 +158,18 @@ def write_trade_log(trade, order_type):
         "order_type": order_type,
         "direction": trade["long_or_short"],
         "price_open": trade["price"],
-        "sl_moved": 0
+        "sl_moved": 0,
+        "sl": trade["sl"]
     }
 
     file_name = "logs/tradelog.csv"
     
     if not os.path.isfile(file_name):
         with open(file_name, "a", newline="", encoding="utf-8") as f:
-            writer = csv.DictWriter( f, ["time", "symbol", "ticket_id", "order_type", "direction", "price_open", "sl_moved"])
+            writer = csv.DictWriter( f, ["time", "symbol", "ticket_id", "order_type", "direction", "price_open", "sl_moved", "sl"])
             writer.writeheader()
 
 
     with open(file_name, "a", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter( f,["time", "symbol", "ticket_id", "order_type", "direction", "price_open", "sl_moved"])
+        writer = csv.DictWriter( f,["time", "symbol", "ticket_id", "order_type", "direction", "price_open", "sl_moved", "sl"])
         writer.writerow(data)
